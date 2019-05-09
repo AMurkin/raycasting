0c61aafc1f2c90c7f2238a5b11d4d95644fce4fd
AMurkin <amurkin1988@yandex.ru>
2019-05-09 14:04:32 +0400

    several walls

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 // 2D visibility
 // Ray casting
 
-let wall;
+let walls = [];
+const walls_count = 5;
 let ray;
 
 function setup() {
     createCanvas(400, 400);
-    wall = new Boundary(300, 100, 300, 300);
+    let wall_start_x = random(width);
+    let wall_start_y = random(height);
+    for(let i=0; i<walls_count; i++) {
+        const wall_end_x = random(width);
+        const wall_end_y = random(height);
+        
+        walls[i] = new Boundary(wall_start_x, wall_start_y, wall_end_x, wall_end_y);
+        
+        wall_start_x = wall_end_x;
+        wall_start_y = wall_end_y;
+    }
     ray = new Ray( 100, 200, 1, 0);
 }
 
 function draw() {
     background(0);
-    wall.show();
-    ray.show();
 
     ray.lookAt(mouseX, mouseY);
-    let pt = ray.cast(wall);
-    if(pt) {
-        stroke(255);
-        noFill();
-        ellipse(pt.x, pt.y, 8, 8);
+    ray.show();
+
+    for(let wall of walls) {
+        wall.show();
+        let pt = ray.cast(wall);
+        if(pt) {
+            stroke(255);
+            noFill();
+            ellipse(pt.x, pt.y, 8, 8);
+        }
     }
 }
